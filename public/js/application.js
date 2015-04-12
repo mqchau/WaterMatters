b25fbdda3479a6005785c9e3c0f1d833f2c92167
Hassen <chaieb.hassen@yahoo.com>
2015-04-11 18:05:09 -0700

    -

--- public/js/application.js
+++ public/js/application.js
@@ -1,20 +1,23 @@
-$('.dropdown-toggle').dropdown()
 
-$('#myDropdown').children().on('click', function () {
-  console.log("55555")
-})
-
-
+$('.dropdown-toggle').dropdown();
 
 var wm = {};
 
+wm.showData = function (pageName){
+	$('#dataPage').show();
+};
+
+
 wm.getCountyData = function (){
 		$.ajax({
-			type: 'POST', // added,
-			url: '/login',
-			data: {"data": "getCountyData"},
+			type: 'GET', // added,
+			url: '/ajaxget',
+			data: {
+				functionName: "getCountyList"
+			},
 			success: function (data) {
 				console.log(data);
+				$("#AjaxGetReturn").html(data);
 			},
 			error: function (xhr, status, error) {
 				console.log('Error: ' + error.message);
@@ -25,14 +28,38 @@ wm.getCountyData = function (){
 
 wm.getStateData = function (){
 		$.ajax({
-			type: 'POST', // added,
-			url: '/login',
-			data: {"data": "getStateData"},
+			type: 'GET', // added,
+			url: '/ajaxget',
+			data: {
+				functionName: "getStateList"
+			},
 			success: function (data) {
-				console.log(data);
+				$("#AjaxGetReturn").html(data);
+				var stateList  = JSON.parse(data).StateList;
+				for (var i = 0; i < stateList.length; i++) {
+					$('#myDropdown').append('<li role="presentation"><a role="menuitem" param='+i+' tabindex="-1" href="#">' + stateList[i].name + '</a></li>');
+				};
+				for (var k = 0; k < stateList.length; k++) {
+					$('#myDropdown').children().eq(k).on('click', function (j) { 
+						var index = j;
+						return function() {
+							console.log(stateList[j].abbreviation);
+							wm.showData();
+						};
+					}(k));
+				};
 			},
 			error: function (xhr, status, error) {
 				console.log('Error: ' + error.message);
 			}
 		});
 };
+
+wm.getStateData();
+
+
+// $('#homeButton').click(function(){wm.changePage("homePage")});
+
+
+
+
