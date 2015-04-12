f631ccf1e8f16a9a1aec1ae0b9ad265b194057ca
Quan Chau <quanqchau@gmail.com>
2015-04-12 12:52:38 -0700

    add water consumption breakdown data from Minh

---
+++ public/js/WaterConsumptionBreakdown.js
@@ -0,0 +1,47 @@
+var WaterConsumptionBreakdown = [
+	{	"Name": "Bath",
+		"Consumption": 0.04,
+		"Solution": "Shower instead of bath",
+		"Saving": 0.04
+	},
+	{	"Name": "Shower",
+		"Consumption": 0.4,
+		"Solution": "Echo Shower",
+		"Saving": 0.2
+	},
+	{	"Name": "Brush teeth",
+		"Consumption":	0.01,
+		"Solution": "",
+		"Saving": 0
+	},
+	{	"Name": "HandWash",
+		"Consumption": 0.02,
+		"Solution": "",
+		"Saving": 0
+	},
+	{	"Name": "Shaving",
+		"Consumption": 0.01,
+		"Solution": "",
+		"Saving": 0
+	},
+	{	"Name": "Clothes washing",
+		"Consumption": 0.16,
+		"Solution": "Wash twice a week",
+		"Saving": 0.08
+	},
+	{	"Name": "ToiletFlush",
+		"Consumption": 0.12,
+		"Solution": "Use eco-friendly toilet",
+		"Saving": 0.06
+	},
+	{	"Name": "Drink",
+		"Consumption": 0.00,
+		"Solution": "",
+		"Saving": 0
+	},
+	{	"Name": "Outdoor Watering",
+		"Consumption": 0.24,
+		"Solution": "divided by 2, night time",
+		"Saving": 0.12
+	}
+];
