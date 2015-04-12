4777957e0b58e0a94c2e7c4ca6eb13c47d428208
Quan Chau <quanqchau@gmail.com>
2015-04-12 12:38:02 -0700

    add data of water problem solution from brandon

---
+++ public/js/WaterProblemSolution.js
@@ -0,0 +1,115 @@
+var WaterProblemSolution = 
+	[{
+		"Problem": "Shower",
+		"Consumption": {"lower": 9, "upper": 12},
+		"Solutions": [
+			{ "Name": "Using water-saving showerheads",
+				"Save": 4800/365
+			},
+			{ "Name": "5 minute showers instead of 10 minute showers",
+				"Save": 12.5
+			}]
+	},
+	{
+		"Problem": "Bath",
+		"Consumption": {"lower": 40, "upper": 40},
+		"Solutions": [
+			{ "Name": "Reduce amount of water in bath",
+				"Save": 12
+			},
+			{ "Name": "Repair faucet leaks",
+				"Save": 15
+			}
+			]
+	},
+	{
+		"Problem": "Watering the lawn",
+		"Consumption": {"lower": 100, "upper": 100},
+		"Solutions": [
+			{ "Name": "Use a broom to sweep the lawn",
+				"Save": 100
+			},
+			{ "Name": "5 minute showers instead of 10 minute showers",
+				"Save": 9300/356
+			}]
+	},
+	{
+		"Problem": "Washing dishes (machine)",
+		"Consumption": {"lower": 13, "upper": 19},
+		"Solutions": [
+			{ "Name": "Running full dishwasher",
+				"Save": 2400/365 
+			}]
+	},
+	{
+		"Problem": "Washing clothes",
+		"Consumption": {"lower": 35, "upper": 50},
+		"Solutions": [
+			{ "Name": "Running a full load in washer",
+				"Save": 2400/365
+			}]
+	},
+	{
+		"Problem": "Washing the car",
+		"Consumption": {"lower": 50, "upper": 50},
+		"Solutions": [
+			{ "Name": "Use a water saving hose nozzle",
+				"Save": 4800/365
+			},
+			{ "Name": "se a commercial car wash instead of washing your car at home",
+				"Save": 40
+			}]
+	},
+	{
+		"Problem": "Brushing your teeth",
+		"Consumption": {"lower": 2, "upper": 5},
+		"Solutions": [
+			{ "Name": "Limit running water",
+				"Save": 5
+			}]
+	},
+	{
+		"Problem": "Cooking",
+		"Consumption": {"lower": 5, "upper": 10},
+		"Solutions": [
+			{ "Name": "Wash Vegetables in a single tub of water",
+				"Save": 2 
+			}]
+	},
+	{
+		"Problem": "Flushing the toilet (once)",
+		"Consumption": {"lower": 4, "upper": 7},
+		"Solutions": [
+			{ "Name": "get low flush toilet",
+				"Save": 4 
+			}]
+	},
+	{
+		"Problem": "Leaking toilet (per day)",
+		"Consumption": {"lower": 60, "upper": 60},
+		"Solutions": [
+			{ "Name": "Fix toilets",
+				"Save": 45 
+			}]
+	},
+	{
+		"Problem": "Watering Plants (per 1000 sq feet)",
+		"Consumption": {"lower": 620, "upper": 620},
+		"Solutions": [
+			{ "Name": "Install a smart sprinkler irrigation controller",
+				"Save": 150 
+			},
+			{ "Name": "Use a water saving hose nozzle",
+				"Save": 20.5 
+			},
+			{ "Name": "Water plants in the morning",
+				"Save": 25 
+			},
+			{ "Name": "Check for sprinkler leaks, overspray & broken sprinkler heads",
+				"Save": 16.4 
+			}
+		]
+	}
+
+
+	];
