ec41062005245e35654e1ed67d582bd2d939cbf5
Hassen <chaieb.hassen@yahoo.com>
2015-04-11 19:23:22 -0700

    -

--- public/js/application.js
+++ public/js/application.js
@@ -8,21 +8,36 @@ wm.showData = function (pageName){
 };
 
 
-wm.getCountyData = function (){
-		$.ajax({
-			type: 'GET', // added,
-			url: '/ajaxget',
-			data: {
-				functionName: "getCountyList"
-			},
-			success: function (data) {
-				console.log(data);
-				$("#AjaxGetReturn").html(data);
-			},
-			error: function (xhr, status, error) {
-				console.log('Error: ' + error.message);
-			}
-		});
+wm.getCountyData = function (countyAb){
+	$.ajax({
+		type: 'GET', 
+		url: '/ajaxget',
+		data: {
+			functionName: "getCountyList",
+			abbreviation: countyAb
+		},
+		success: function (data) {
+			var countyList  = JSON.parse(data).CountyList;
+			$('#myDropdownCounty').parent().show();
+			for (var i = 0; i < countyList.length; i++) {
+				$('#myDropdownCounty').append('<li role="presentation"><a role="menuitem" param='+i+' tabindex="-1" href="#">' + countyList[i] + '</a></li>');
+			};
+			for (var k = 0; k < countyList.length; k++) {
+					$('#myDropdownCounty').children().eq(k).on('click', function (j) { 
+						var index = j;
+						return function() {
+							$('#textLabelCounty').html(countyList[j])
+							console.log(countyList[j]);
+							wm.showData();
+						};
+					}(k));
+			};
+			$("#AjaxGetReturn").html(data);
+		},
+		error: function (xhr, status, error) {
+			console.log('Error: ' + error.message);
+		}
+	});
 };
 
 
@@ -39,12 +54,14 @@ wm.getStateData = function (){
 				for (var i = 0; i < stateList.length; i++) {
 					$('#myDropdown').append('<li role="presentation"><a role="menuitem" param='+i+' tabindex="-1" href="#">' + stateList[i].name + '</a></li>');
 				};
+
+
 				for (var k = 0; k < stateList.length; k++) {
 					$('#myDropdown').children().eq(k).on('click', function (j) { 
 						var index = j;
 						return function() {
 							console.log(stateList[j].abbreviation);
-							wm.showData();
+							wm.getCountyData(stateList[j].abbreviation);
 						};
 					}(k));
 				};
@@ -58,8 +75,24 @@ wm.getStateData = function (){
 wm.getStateData();
 
 
-// $('#homeButton').click(function(){wm.changePage("homePage")});
-
+$('#localButton').click(function(){
+	$.ajax({
+		type: 'POST', // added,
+		url: '/login',
+		data: {"data": "TEST"},
+		success: function (data) {
+			console.log(data);
+			$('#localButton').after('<br>'+data);
+			//var ret = jQuery.parseJSON(data);
+			//$('#lblResponse').html(ret.msg);
+			$("#LocationData").html(data);
+		},
+		error: function (xhr, status, error) {
+			console.log('Error: ' + error.message);
+			$('#lblResponse').html('Error connecting to the server.');
+		}
+	});
+});
 
 
 
